620c1d2684fc6444dd9a548db5cd8e66cbab6de9
Naimisha Badri <starzblue24@gmail.com>
2015-03-09 19:40:15 -0700

    Update CardTester.java

--- src/CardTester.java
+++ src/CardTester.java
@@ -2,12 +2,18 @@
  * This is a class that tests the Card class.
  */
 public class CardTester {
-
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+  
+  /**
+   * The main method in this class checks the Card operations for consistency.
+   * @param args is not used.
+   */
+  public static void main(String[] args) {
+    Card card1 = new Card("Ace", "Clubs", 1);
+    Card card2 = new Card("King", "Spade", 13);
+    Card card3 = new Card("Queen", "Hearts", 12);
+    System.out.println(card2.matches(card1));
+    System.out.print(card1.toString());
+    
+    /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+  }
 }
